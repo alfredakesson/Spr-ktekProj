196588d42857db0be3f6545d4f7077fd812ddfaa
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-05 15:34:00 +0100

    Lite regex

--- sprakproj/src/sprakproj/Startclass.java
+++ sprakproj/src/sprakproj/Startclass.java
@@ -1,5 +1,6 @@
 package sprakproj;
 
+import java.util.LinkedList;
 import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
@@ -10,54 +11,52 @@ import info.bliki.wiki.dump.WikiXMLParser;
 
 import org.xml.sax.SAXException;
 
+public class Startclass implements IArticleFilter {
 
+	private Pattern r;
+	public LinkedList<String> res;
+	public int num = 0;
 
-public class Startclass {
-    static class DemoArticleFilter implements IArticleFilter {
+	public Startclass() {
+		String pattern = "[f|F]öd.{1,10} \\s*=\\s*(.*)";
+		r = Pattern.compile(pattern);
+		res = new LinkedList<String>();
+	}
 
-        @Override
-        public void process(WikiArticle page, Siteinfo siteinfo) throws SAXException {
-        	
-            // String to be scanned to find the pattern.
-            String line = page.getText();
-            String pattern = "född_datum \\s*=\\s*(.*)";
-
-            // Create a Pattern object
-            Pattern r = Pattern.compile(pattern);
-
-            // Now create matcher object.
-            Matcher m = r.matcher(line);
-            if (m.find( )) {
-            	System.out.println(page.getTitle());
-            	System.out.println("Found value: " + m.group(1) );
-            }
-            
-        }
-
-
-}
-
-	public static void main(String[] args) {
-    String bz2Filename = "../../svwiki-20131101-pages-articles.xml.bz2";
-    System.out.println("hej");
-    try {
-            IArticleFilter handler = new DemoArticleFilter();
-            WikiXMLParser wxp = new WikiXMLParser(bz2Filename, handler);
-            wxp.parse();
-    } catch (Exception e) {
-            e.printStackTrace();
-    }
+	@Override
+	public void process(WikiArticle page, Siteinfo siteinfo)
+			throws SAXException {
+		Matcher m = r.matcher(page.getText());
+		if (m.find()) {
+			res.add(page.getTitle() + m.group(1));
+		}
+		if(num > 10000){
+			throw new SAXException();
+		}
+		num++;
 
 	}
 
-	
+	public static void main(String[] args) {
+		String bz2Filename = "../../svwiki-20131101-pages-articles.xml.bz2";
+		System.out.println("hej");
+		Startclass handler = new Startclass();
+		try {
+			WikiXMLParser wxp = new WikiXMLParser(bz2Filename, handler);
+			wxp.parse();
+		} catch (Exception e) {
+			e.printStackTrace();
+			System.out.println(handler.res.size());
+			for (String theRes : handler.res) {
+				System.out.println(theRes);
+				
+			}
+		}
+
+	}
+
 	/*
-	 * född
-	 * födelseår
-	 * födelsedatum
-	 * 
-	 * 
-	 * 
+	 * född födelseår födelsedatum
 	 */
-	
+
 }
