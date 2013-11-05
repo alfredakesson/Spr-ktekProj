eab614fdd1f20b1214b69e7adf0bb918b69490bf
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-05 16:49:37 +0100

    Mer grejor

--- sprakproj/src/sprakproj/Startclass.java
+++ sprakproj/src/sprakproj/Startclass.java
@@ -16,10 +16,17 @@ public class Startclass implements IArticleFilter {
 	private Pattern r;
 	public LinkedList<String> res;
 	public int num = 0;
+	private Pattern yr;
+	private Pattern dr;
 
 	public Startclass() {
 		String pattern = "[f|F]öd.{1,10} \\s*=\\s*(.*)";
+		String yearPattern = "\\[\\[\\s{0,3}(\\d{4})\\s{0,3}\\]\\]"; //"\\[\\[\\s{1,3}(\\d{1,2})\\s{0,3}(\\w{2,10})\\]\\]//\\s{0,3}\\[\\[(\\d{4})\\]\\]";
+		String datePattern = "\\[\\[\\s{0,3}(\\d{1,2})\\s{0,3}(\\w{2,10})\\s{0,3}\\]\\]";
 		r = Pattern.compile(pattern);
+		yr = Pattern.compile(yearPattern);
+		dr = Pattern.compile(datePattern);
+		
 		res = new LinkedList<String>();
 	}
 
@@ -28,7 +35,22 @@ public class Startclass implements IArticleFilter {
 			throws SAXException {
 		Matcher m = r.matcher(page.getText());
 		if (m.find()) {
-			res.add(page.getTitle() + m.group(1));
+			
+			
+			String dateS = m.group(1);
+			Matcher ym = yr.matcher(dateS);
+			
+			if(ym.find()){
+				res.add(page.getTitle() + m.group(1));
+				System.out.print(page.getTitle()+ " föddes " + ym.group(1));
+				Matcher dm = dr.matcher(dateS);
+				if(dm.find()){
+					System.out.print(" månad " + dm.group(2) + " dag " + dm.group(1));
+				}
+				System.out.println();
+				
+			}
+
 		}
 		if(num > 10000){
 			throw new SAXException();
@@ -47,16 +69,20 @@ public class Startclass implements IArticleFilter {
 		} catch (Exception e) {
 			e.printStackTrace();
 			System.out.println(handler.res.size());
-			for (String theRes : handler.res) {
-				System.out.println(theRes);
-				
-			}
+//			for (String theRes : handler.res) {
+//				System.out.println(theRes);
+//				
+//			}
 		}
 
 	}
 
 	/*
 	 * född födelseår födelsedatum
+	 * 
+	 * 
+	 *[[5 oktober]] [[1785]]
+	 *{{Ålder|1973|6|6}}
 	 */
 
 }
