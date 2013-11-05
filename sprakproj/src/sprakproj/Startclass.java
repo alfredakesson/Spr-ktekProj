994c16d3cf0f33e700c55d04dee585aa88dc6192
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-05 14:06:17 +0100

    Här kommmer biblokeket

--- sprakproj/src/sprakproj/Startclass.java
+++ sprakproj/src/sprakproj/Startclass.java
@@ -1,11 +1,40 @@
 package sprakproj;
 
+import info.bliki.wiki.dump.IArticleFilter;
+import info.bliki.wiki.dump.Siteinfo;
+import info.bliki.wiki.dump.WikiArticle;
+import info.bliki.wiki.dump.WikiXMLParser;
+
+import org.xml.sax.SAXException;
+
 
 
 public class Startclass {
-	
+    static class DemoArticleFilter implements IArticleFilter {
+
+        @Override
+        public void process(WikiArticle page, Siteinfo siteinfo) throws SAXException {
+                System.out.println("----------------------------------------");
+                System.out.println(page.getId());
+                System.out.println(page.getRevisionId());
+                System.out.println(page.getTitle());
+                System.out.println("----------------------------------------");
+                System.out.println(page.getText());
+        }
+
+
+}
+
 	public static void main(String[] args) {
-		System.out.println("hej");
+    String bz2Filename = "/Users/alfredakesson/Downloads/svwiki-20131101-pages-articles.xml.bz2";
+    System.out.println("hej");
+    try {
+            IArticleFilter handler = new DemoArticleFilter();
+            WikiXMLParser wxp = new WikiXMLParser(bz2Filename, handler);
+            wxp.parse();
+    } catch (Exception e) {
+            e.printStackTrace();
+    }
 
 	}
 
