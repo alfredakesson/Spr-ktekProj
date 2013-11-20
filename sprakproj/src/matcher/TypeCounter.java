66dffe7c4ce014a782a31ff8f2b569e44f90bd4f
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-20 16:05:23 +0100

    addes

---
+++ sprakproj/src/matcher/TypeCounter.java
@@ -0,0 +1,29 @@
+package matcher;
+
+import org.sweble.wikitext.lazy.preprocessor.TemplateArgument;
+
+import sprakproj.Database;
+
+public class TypeCounter implements PossibleMatch {
+	Database db;
+	
+	public TypeCounter(){
+		db = Database.getInstance();
+	}
+
+	@Override
+	public void saveStringToDb(String wikiName, String wikiValue,
+			String pageTitle, TemplateArgument templateArgument) {
+			db.insertTriple(wikiName.trim(), pageTitle, wikiValue, "typeCounter");
+		
+		
+
+	}
+
+	@Override
+	public boolean foundPattern(String wikiText) {
+		// TODO Auto-generated method stub
+		return true;
+	}
+
+}
