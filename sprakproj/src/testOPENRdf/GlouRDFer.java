17166f16430f3b92cb5c3df6102f9b17c0c908f5
Alfred <Alfred@Alfreds-Dator>
2013-11-27 20:49:58 +0100

    close

--- sprakproj/src/testOPENRdf/GlouRDFer.java
+++ sprakproj/src/testOPENRdf/GlouRDFer.java
@@ -2,6 +2,8 @@ package testOPENRdf;
 
 import java.io.File;
 import java.io.IOException;
+import java.sql.ResultSet;
+import java.sql.SQLException;
 
 import org.openrdf.query.MalformedQueryException;
 import org.openrdf.query.QueryEvaluationException;
@@ -13,7 +15,7 @@ import org.openrdf.rio.RDFParseException;
 import org.openrdf.sail.nativerdf.NativeStore;
 
 public class GlouRDFer {
-	public static void main(String args[]) throws RepositoryException, RDFParseException, IOException, MalformedQueryException, QueryEvaluationException {
+	public static void main(String args[]) throws RepositoryException, RDFParseException, IOException, MalformedQueryException, QueryEvaluationException, SQLException {
 		File dataDir = new File(".");
 		Repository repo = new SailRepository(new NativeStore(dataDir));
 		repo.initialize();
@@ -26,6 +28,16 @@ public class GlouRDFer {
 		for(String s: kolla){
 			System.out.println(s);
 		}
-		
+		Database db = Database.getInstance();
+		ResultSet rs = db.getTable();
+		int num = 0;
+		while(rs.next()){
+			if(num%1000 == 0){
+				System.out.println(num);
+			}
+			num++;
+		}
+		conn.close();
+
 	}
 }
