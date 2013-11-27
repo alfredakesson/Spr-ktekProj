a617ddfe4340f166cd9805662e987a1cea6974df
Alfred <Alfred@Alfreds-Dator>
2013-11-27 21:50:44 +0100

    getProp

--- sprakproj/src/testOPENRdf/GlouRDFer.java
+++ sprakproj/src/testOPENRdf/GlouRDFer.java
@@ -20,41 +20,51 @@ import org.openrdf.rio.RDFParseException;
 import org.openrdf.sail.nativerdf.NativeStore;
 
 public class GlouRDFer {
-	public static void main(String args[]) throws RepositoryException, RDFParseException, IOException, MalformedQueryException, QueryEvaluationException, SQLException {
+	public static void main(String args[]) throws RepositoryException,
+			RDFParseException, IOException, MalformedQueryException,
+			QueryEvaluationException, SQLException {
 
 		File dataDir = new File(".");
 		Repository repo = new SailRepository(new NativeStore(dataDir));
 		repo.initialize();
 		RepositoryConnection conn = repo.getConnection();
-		
-		DbPediaQuestion dbQ =new DbPediaQuestion(conn);
-		
+
+		DbPediaQuestion dbQ = new DbPediaQuestion(conn);
+
 		String exist = dbQ.existArticle("Sverige");
 		System.out.println(exist);
 		String[] kolla = testOpenRDF.getTypes(exist, conn);
-		for(String s: kolla){
+		for (String s : kolla) {
 			System.out.println(s);
 		}
 		Database db = Database.getInstance();
 		ResultSet rs = db.getTable();
 		int num = 0;
-		while(rs.next()){
-			if(num%1000 == 0){
+		while (rs.next()) {
+			if (num % 1000 == 0) {
 				System.out.println(num);
 			}
 			num++;
 		}
+		String getProp = "\\[\\[(.+?)(\\]\\]|\\|)";
 		conn.close();
 
 	}
 
 	private static void inset_type_prop_lit(Repository repo,
-			RepositoryConnection conn, String type , String prop , String prop_val) throws RepositoryException {
+			RepositoryConnection conn, String[] types, String prop,
+			String[] prop_vals) throws RepositoryException {
 		ValueFactory factory = repo.getValueFactory();
-		URI type_URI = factory.createURI(type);
-		URI prop_URI = factory.createURI("http://scn.cs.lth.se/rawproperty/" + prop);
-		Literal prop_value_lit = factory.createLiteral(prop_val);
-		Statement type_prop_lit = factory.createStatement(type_URI, prop_URI, prop_value_lit);
-		conn.add(type_prop_lit);
+		for (String prop_val : prop_vals) {
+			for (String type : types) {
+				URI type_URI = factory.createURI(type);
+				URI prop_URI = factory
+						.createURI("http://scn.cs.lth.se/rawproperty/" + prop);
+				URI prop_value_URI = factory.createURI(prop_val);
+				Statement type_prop_uri = factory.createStatement(type_URI,
+						prop_URI, prop_value_URI);
+				conn.add(type_prop_uri);
+			}
+		}
 	}
 }
