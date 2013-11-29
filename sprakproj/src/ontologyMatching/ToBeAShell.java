f0c83c8f22c654d41af58cf0f96e1db084c78ca3
Alfred Åkesson <alfred.akesson@tele2.se>
2013-11-29 13:23:52 +0100

    hej

--- sprakproj/src/ontologyMatching/ToBeAShell.java
+++ sprakproj/src/ontologyMatching/ToBeAShell.java
@@ -4,13 +4,13 @@ public class ToBeAShell {
 
 	public static void main(String[] args) {
 		String queryString = "SELECT ?type WHERE {"
-				+ "?hej <http://scn.cs.lth.se/rawproperty/födelsedatum> ?type."
+				+ "<http://dbpedia.org/resource/Greta_Garbo> <http://scn.cs.lth.se/rawproperty/dödsplats> ?type."
 				//+ " ?type <http://www.w3.org/1999/02/22-rdf-syntax-ns#type> <http://www.w3.org/2002/07/owl#Class> ."
 				+ "}";
 		SesameDb db = new SesameDb();
 		db.createDb();
 		for( String s:db.askSesame(queryString, "type")){
-			System.out.println(s);
+			System.out.println();
 		};
 	}
 
